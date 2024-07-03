295de735f36e5d18fd6363d783a7954e480b1458
Pourya <pourya.nejadensan@gmail.com>
2024-07-03 15:12:36 +0200

    step3: add Library class with an array of Book class

---
+++ src/main/java/Library.java
@@ -0,0 +1,11 @@
+public class Library {
+    Book[] books;
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+}
