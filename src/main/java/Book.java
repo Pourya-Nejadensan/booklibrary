82251c1cb9750a8b4b162dc21b6b2745c0011f8e
Pourya <pourya.nejadensan@gmail.com>
2024-07-03 15:07:56 +0200

    step1: add Book class with getter and setter

---
+++ src/main/java/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    String title;
+    String author;
+    String isbn;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
